641b919386143b1e9035c60367d3cae2f8ea2378
student <dragoontooth123@gmail.com>
2018-05-31 18:14:24 +0800

    Correction

--- server.js
+++ server.js
@@ -102,6 +102,6 @@ server.get('/form',(req, res) => {
   res.render('form.hbs');
 });
 
-server.listen(3000,() => {
+server.listen(port,() => {
   console.log(`Server listening on port ${port}");
 });
